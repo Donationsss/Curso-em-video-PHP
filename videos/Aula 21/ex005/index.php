011ca4a1f5ffb08443271c193235bd43313bd6f0
Donassan <163194656+Donationsss@users.noreply.github.com>
2025-08-06 16:45:11 -0300

    Add files via upload

---
+++ videos/Aula 21/ex005/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 21 - Tipos Primitivos PHP</title>
+</head>
+<body>
+    <h1>Teste de Tipos Primitivos</h1>
+    <?php 
+
+        // Tipos primitivos escalares
+        
+        // 0x = hexadecimal 0b = binário 0 = octal
+        // $num = 0x1A;
+        // echo "O valor da variável é $num";
+
+        // $valor = 100;
+        // $valor2 = "Hello";
+        // $valor3 = 1.2;
+        // $valor4 = true;
+        // var_dump($valor, $valor2, $valor3, $valor4);
+
+        //$num = (int) 3e2; // -> 3 x 10^2
+        // echo "O valor é $num";
+        //var_dump($num);
+
+        // $num = (float) "17";
+        // var_dump($num);
+
+        // $casado = true;  -> se for exibir o tipo booleano na página para que o usuário veja, ele não será exibido true e false, mas sim como 1 e vazio.
+        // $casado2 = false; 
+        // var_dump($casado);
+        // echo "O valor para casado é $casado";
+        // echo "O valor para casado é $casado2";
+
+        // $vet = [0, 2, "Enzo", true];
+        // var_dump($vet);
+
+        // class Pessoa {
+        //     private string $nome; 
+        // }
+
+        // $p = new Pessoa;
+        // var_dump($p);
+
+    ?>
+</body>
+</html>
